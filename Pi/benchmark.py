d4c72854918c060e4a07276597b20ab53fca83b0
DelSquared <37172200+DelSquared@users.noreply.github.com>
2018-12-16 20:27:18 +0100

    Update benchmark.py

--- Pi/benchmark.py
+++ Pi/benchmark.py
@@ -3,9 +3,9 @@ from cy.cyPi import Pi as cypi
 from py.pyPi import Pi as pypi
 import time
 
-n=100000000
+n=1000000000
 
-print("finding pi with {} samples".format(n))
+print("Finding pi with {} samples".format(n))
 
 print("Starting Cy...\n")
 t0 = time.time()
@@ -21,7 +21,7 @@ t1 = time.time()
 
 totalpy = t1-t0
 
-print("cython value = {}\nptyhon value = {}".format(cy,py))            
+print("Cython value = {}\nPython value = {}\n".format(cy,py))            
 
 print("Cython: {:.5f}\nPython: {:.5f}\n".format(totalcy,totalpy))
 print("Cython is {:.4f}x faster than Python".format(totalpy/totalcy))
