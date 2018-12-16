a97cd4804fa6021a2f533affc2eb6a991b2d0388
DelSquared <37172200+DelSquared@users.noreply.github.com>
2018-12-16 20:27:47 +0100

    Update cyPi.pyx

--- Pi/cy/cyPi.pyx
+++ Pi/cy/cyPi.pyx
@@ -3,13 +3,13 @@ from libc.stdlib cimport RAND_MAX
 from cython.parallel import prange
 
 cpdef double Pi(int n):
-   cdef int c=0
-   cdef int i
-   cdef float x
-   cdef float y
-   for i in prange(n, nogil=True):
-      x = rand()/RAND_MAX
-      y = rand()/RAND_MAX
-      if x*x+y*y<1:
+   cdef long c=0
+   cdef long i
+   cdef double x
+   cdef double y
+   for i in prange(n, nogil=True,num_threads=6):
+      x = <double>rand()/<double>RAND_MAX
+      y = <double>rand()/<double>RAND_MAX
+      if (x*x+y*y)<1:
          c+=1
-   return 4*<double>c/<double>n
+   return 4*<double>(c)/<double>n
