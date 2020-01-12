ca43e3a0b336cae5a161901103148c792253096a
DelSquared <37172200+DelSquared@users.noreply.github.com>
2020-01-12 11:55:31 +0100

    Update cysqrt.pyx

--- Fast Inverse Square Root -Newton-Raphson/cy/cysqrt.pyx
+++ Fast Inverse Square Root -Newton-Raphson/cy/cysqrt.pyx
@@ -1,16 +1,22 @@
-cpdef invsqrt(x):
-    cdef int i
-    cdef float x2, y
+cdef extern from *:
+    """
+    //Yanked from the Quake III Arena source code as provided on the Wikipedia article
+    float c_invsqrt( float number ){
+        long i;
+        float x2, y;
 
-    x2 = x* 0.5
-    y  = x
+        x2 = number * 0.5F;
+        y  = number;
+        i  = * ( long * ) &y;
+        i  = 0x5f3759df - ( i >> 1 );
+        y  = * ( float * ) &i;
+        y  = y * (1.5F - ( x2 * y * y ) );
+        return y;
+    }
 
-    i  = (<long*> &y)[0]
-    i  = 0x5f3759df - ( i >> 1 )
-    y  = (<float*> &i)[0]
+    """
+    float c_invsqrt(float number)
 
-    cdef int k
-    for k in range(1000):
-        y  = y * ( 1.5 - ( x2 * y * y ) )
 
-    return y
+cpdef float invsqrt(float number):
+    return c_invsqrt(number)
